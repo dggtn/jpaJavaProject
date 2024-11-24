3a6b65591bcb60a9ebdab9e8869814200f4861fa
daniela <dggtn97@gmail.com>
2024-11-24 20:04:20 -0300

    agrego package test y dependencias a pom.xml

--- src/test/java/com/aluracursos/screenmatch/ScreenmatchTest.java
+++ src/test/java/com/aluracursos/screenmatch/ScreenmatchTest.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ScreenmatchApplicationTests {
+public class ScreenmatchTest {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
